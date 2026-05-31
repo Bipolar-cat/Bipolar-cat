f7f506f7eb03b8fdc25eed327155e77d26e29d4b
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-05-31 18:40:35 +0900

    Delete test/data.js

--- test/data.js
+++
@@ -1,9 +0,0 @@
-// data.js
-const DataManager = {
-    saveDiagnosis(name) {
-        localStorage.setItem('userDiagnosis', name);
-    },
-    getDiagnosis() {
-        return localStorage.getItem('userDiagnosis') || '未選択';
-    }
-};
