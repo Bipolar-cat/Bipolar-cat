74d2908f7000490c0eb2a6168617ab94c8eeab10
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-01 23:48:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, Render!"
+def index():
+    return render_'step3(index.html')
 
 if __name__ == '__main__':
     app.run()
