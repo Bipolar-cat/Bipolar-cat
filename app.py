49e290f0e614adb8c0fefbc519b06ba67f456491
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-02 19:28:26 +0900

    Add multiple routes for test and summary pages

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-@app.route('/')
+@app.route('/test')
 def index():
-    # 'templates' フォルダの中の index.html を探す
-    return render_template('index.html') 
+    return render_template('test/index.html')
+
+@app.route('/step3')
+def step3():
+    return render_template('base/step3/index.html')
+
+@app.route('/step10')
+def step10():
+    return render_template('base/step10/index.html')
+
+@app.route('/summary/step3')
+def summary_step3():
+    return render_template('summary/step3/index.html')
+
+@app.route('/summary/step10')
+def summary_step10():
+    return render_template('summary/step10/index.html')
+
+@app.route('/karte/step3')
+def karte_step3():
+    return render_template('karte/step3/index.html')
+
+@app.route('/karte/step10')
+def karte_step10():
+    return render_template('karte/step10/index.html')
