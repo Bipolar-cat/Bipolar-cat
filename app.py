a5ec6bae186e191defa47e6ed851ea97b1803c8a
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-02 19:13:47 +0900

    Change template rendering to index.html
    Updated the Flask app to render index.html from the templates folder.

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='base')
-
 @app.route('/')
 def index():
-    return render_template('step3/index.html')
-
-if __name__ == '__main__':
-    app.run()
+    # 'templates' フォルダの中の index.html を探す
+    return render_template('index.html') 
