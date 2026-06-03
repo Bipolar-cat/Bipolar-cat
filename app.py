37f647880d174861df4708b82043fad40b077b14
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-03 18:03:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# ルートの定義をすべて app.run() の前に移動しました
-@app.route('/')
-@app.route('/test')
-def index():
-    return render_template('test/index.html') 
+# --- Karteのルート ---
+@app.route('/karte/step3')
+def karte_step3():
+    return render_template('karte/step3/index.html')
 
-@app.route('/step3')
-def step3():
-    return render_template('base/step3/index.html')
+@app.route('/karte/step10')
+def karte_step10():
+    return render_template('karte/step10/index.html')
 
-@app.route('/step10')
-def step10():
-    return render_template('base/step10/index.html')
-
+# --- Summaryのルート ---
 @app.route('/summary/step3')
 def summary_step3():
     return render_template('summary/step3/index.html')
@@ -24,13 +20,19 @@ def summary_step3():
 def summary_step10():
     return render_template('summary/step10/index.html')
 
-@app.route('/karte/step3')
-def karte_step3():
-    return render_template('karte/step3/index.html')
+# --- Baseのルート ---
+@app.route('/base/step3')
+def base_step3():
+    return render_template('base/step3/index.html')
 
-@app.route('/karte/step10')
-def karte_step10():
-    return render_template('karte/step10/index.html')
+@app.route('/base/step10')
+def base_step10():
+    return render_template('base/step10/index.html')
+
+# --- Testのルート ---
+@app.route('/test')
+def test():
+    return render_template('test/index.html')
 
 if __name__ == '__main__':
     app.run()
