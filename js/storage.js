85c8714eab2ed1b5f882979c5c6c4a1b714a7049
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-07 19:48:38 +0900

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,18 @@
+const STORAGE_KEY = 'innernote_vfinal_400_logs';
+const DIAGNOSIS_KEY = 'innernote_saved_diagnosis';
 
+function saveLogs(logs) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(logs));
+}
+
+function loadLogsData() {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+}
+
+function saveDiagnosis(value) {
+    localStorage.setItem(DIAGNOSIS_KEY, value);
+}
+
+function loadDiagnosis() {
+    return localStorage.getItem(DIAGNOSIS_KEY);
+}
