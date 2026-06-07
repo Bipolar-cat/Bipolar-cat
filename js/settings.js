cb40680999b51f90bcf0cc63d491a107c75cab62
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-07 19:50:13 +0900

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,14 @@
+function loadDiagnosisUI() {
+    const saved = loadDiagnosis();
 
+    if (saved) {
+        document.getElementById('diagnosis-select-container').style.display = 'none';
+        document.getElementById('diagnosis-fixed-container').style.display = 'flex';
+        document.getElementById('diagnosis-text').innerText = saved;
+    }
+}
+
+function lockDiagnosis() {
+    const value = document.getElementById('diagnosis-select').value;
+    saveDiagnosis(value);
+}
