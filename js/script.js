271c019dbb3bcd5c433c6feefb28b051cda892aa
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-08 13:29:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
 window.onload = () => {
 
-    loadSettings();
+    function loadSettings() {
+
+    const saved =
+        localStorage.getItem(
+            SETTINGS_KEY
+        );
+
+    if (!saved) return;
+
+    const settings =
+        JSON.parse(saved);
+
+    const mode =
+        settings.recordMode || "10";
+
+    console.log(
+        "mode=",
+        mode
+    );
+
+    if (mode === "3") {
+
+        document.getElementById(
+            "step3-area"
+        ).style.display = "block";
+
+        document.getElementById(
+            "step10-area"
+        ).style.display = "none";
+
+    } else {
+
+        document.getElementById(
+            "step3-area"
+        ).style.display = "none";
+
+        document.getElementById(
+            "step10-area"
+        ).style.display = "block";
+    }
+
 
     loadLogs();
 
