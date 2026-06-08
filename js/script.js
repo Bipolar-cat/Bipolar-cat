4627be859f5ffa4853902220bbe56c574a8b1e94
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-08 19:23:35 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,10 +31,16 @@ function renderStep3Buttons() {
     // 体調ボタンも同様に生成
 }
 
+// 必要な機能をインポート
 import { toggleSettings } from './settings.js';
-import { saveData } from './storage.js'; // 各自のファイルからインポート
+import { saveData } from './storage.js';
+import { lockDiagnosis, unlockDiagnosis } from './settings.js';
 
-// これによりHTMLから呼び出せるようになる
+// HTMLから直接関数を呼べるようにwindowへ登録
 window.toggleSettings = toggleSettings;
 window.saveData = saveData;
-// 必要に応じて他の関数も追加してください
+window.lockDiagnosis = lockDiagnosis;
+window.unlockDiagnosis = unlockDiagnosis;
+
+// その他の初期化処理...
+
