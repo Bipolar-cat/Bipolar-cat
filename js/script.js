92467807542189738ffea981f78b43178251b264
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-10 21:40:44 +0900

    Expose toggleSettings and saveSettings to global scope

--- js/script.js
+++ js/script.js
@@ -11,3 +11,11 @@ window.onload = () => {
     );
 
 };
+
+// js/script.js
+
+// グローバルスコープに各関数を割り当てる（これでonclickから呼び出せるようになります）
+window.toggleSettings = toggleSettings;
+window.saveSettings = saveSettings;
+// 他の関数が必要ならここに追加していく
+// window.saveData = saveData; 
