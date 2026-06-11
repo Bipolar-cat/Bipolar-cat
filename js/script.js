30f68aaa34d5cf5ab367c20c19bfe71e71f3a063
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-11 18:22:09 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
-// state
-let state = loadState();
-
-// 初期化
 window.addEventListener("DOMContentLoaded", () => {
 
-  initMoodButtons();
-  initCondButtons();
+    renderThreeButtons(
+        "mood-buttons",
+        "mood"
+    );
 
-  setupSingleSelect("mood-buttons", "mood");
-  setupSingleSelect("cond-buttons", "cond");
+    renderThreeButtons(
+        "cond-buttons",
+        "cond"
+    );
 
-  applyState();
 });
-
-// ボタン生成
-function initMoodButtons() { ... }
-function initCondButtons() { ... }
-
-// クリック制御
-function setupSingleSelect(containerId, key) { ... }
-
-// state反映
-function applyState() { ... }
