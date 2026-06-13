6a66212b64f7611e4b72a02a5025deae3fc0876a
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-13 10:36:28 +0900

    Remove chart drawing logic from chart.js

--- js/chart.js
+++ js/chart.js
@@ -3,23 +3,3 @@ console.log("chart.js loaded");
 function drawChart(logs){
 
 }
-
-new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: last10.map(l => l.date),
-                    datasets: [
-                        { 
-                            label: '気分', data: last10.map(l => l.mood), 
-                            borderColor: '#2196F3', backgroundColor: '#2196F3', 
-                            borderWidth: 2, tension: 0.3, fill: false, 
-                            pointRadius: 4, pointHitRadius: 15 
-                        },
-                        { 
-                            label: '体調', data: last10.map(l => l.cond), 
-                            borderColor: '#FFA726', backgroundColor: '#FFA726', 
-                            borderWidth: 2, tension: 0.3, fill: false, 
-                            pointRadius: 4, pointHitRadius: 15 
-                        }
-                    ]
-                },
