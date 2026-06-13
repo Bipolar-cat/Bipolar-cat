6300d044f71e91e915a34b63b0ca8f57b769e92a
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-13 22:28:23 +0900

    Update logs.js

--- js/logs.js
+++ js/logs.js
@@ -24,3 +24,21 @@ function saveData() {
 
     }
 }
+function setVal(type,val,btn){
+
+    const parent = btn.parentElement;
+
+    parent
+      .querySelectorAll("button")
+      .forEach(b =>
+          b.classList.remove("active")
+      );
+
+    btn.classList.add("active");
+
+    if(type==="mood"){
+        selectedMood = val;
+    }else{
+        selectedCond = val;
+    }
+}
