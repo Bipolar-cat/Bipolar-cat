cbfff6d57fb3b2d8ee2c5fac99c495566c24eb82
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-13 21:00:14 +0900

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,18 +1,15 @@
-console.log("storage.js loaded");
-
-const STORAGE_KEY =
-  "innernote_3step_logs";
+const STORAGE_KEY = "innernote_3step_logs";
 
 function getLogs() {
-  return JSON.parse(
-    localStorage.getItem(STORAGE_KEY)
-    || "[]"
-  );
+    return JSON.parse(
+        localStorage.getItem(STORAGE_KEY)
+        || "[]"
+    );
 }
 
 function saveLogs(logs) {
-  localStorage.setItem(
-    STORAGE_KEY,
-    JSON.stringify(logs)
-  );
+    localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(logs)
+    );
 }
