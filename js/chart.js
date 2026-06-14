aa10be5f26b166f0c84126305fb1b39a3b0d8494
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-14 19:30:31 +0900

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -17,20 +17,3 @@ new Chart(ctx, {
                         }
                     ]
                 },
-function renderChart() {
-
-   const logs = getLogs();
-
-   const last10 =
-       logs.slice(-10);
-
-   const ctx =
-       document
-       .getElementById("myChart")
-       .getContext("2d");
-
-   new Chart(ctx,{
-      ...
-   });
-
-}
