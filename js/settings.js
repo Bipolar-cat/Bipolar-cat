cd11a694e87675dd1a947f8fef906ea214e04cb2
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-14 23:22:02 +0900

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -13,17 +13,15 @@ function closeSettings(){
 }
 
 function toggleSettings(){
-    alert("設定画面");
-}
 
     const panel =
         document.getElementById(
             "settings-panel"
         );
 
-    if(panel.style.display==="block"){
-        panel.style.display="none";
+    if(panel.style.display === "block"){
+        panel.style.display = "none";
     }else{
-        panel.style.display="block";
+        panel.style.display = "block";
     }
 }
