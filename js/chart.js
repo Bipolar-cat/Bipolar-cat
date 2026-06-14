c844ade8046c9c836bf29b93a299972cd0b135ee
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-15 02:27:10 +0900

    Refactor chart.js to create a line chart with datasets

--- js/chart.js
+++ js/chart.js
@@ -1,23 +1,58 @@
-options: {
-    responsive: true,
-    maintainAspectRatio: false,
+new Chart(ctx, {
+    type: "line",
 
-    scales: {
-        y: {
-            min: 1,
-            max: 3,
-            stepSize: 1,
+    data: {
+        labels: last10.map(l => l.date),
 
-            ticks: {
-                callback: function(value) {
+        datasets: [
+            {
+                label: "気分",
+                data: last10.map(l => l.mood),
+                borderColor: "#2196F3",
+                yAxisID: "yMood"
+            },
+            {
+                label: "体調",
+                data: last10.map(l => l.cond),
+                borderColor: "#FFA726",
+                yAxisID: "yCond"
+            }
+        ]
+    },
 
-                    if (value === 3) return "良い";
-                    if (value === 2) return "普通";
-                    if (value === 1) return "低い／悪い";
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
 
-                    return "";
+        scales: {
+            yMood: {
+                position: "left",
+                min: 1,
+                max: 3,
+                ticks: {
+                    callback: v => {
+                        if (v === 3) return "良い";
+                        if (v === 2) return "普通";
+                        if (v === 1) return "低い";
+                    }
+                }
+            },
+
+            yCond: {
+                position: "right",
+                min: 1,
+                max: 3,
+                grid: {
+                    drawOnChartArea: false
+                },
+                ticks: {
+                    callback: v => {
+                        if (v === 3) return "良い";
+                        if (v === 2) return "普通";
+                        if (v === 1) return "悪い";
+                    }
                 }
             }
         }
     }
-}
+});
