26662b9f126f30f90cc6b2eeeef87773eec848d7
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-23 16:16:34 +0900

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -206,10 +206,9 @@ if (chartInner) {
                 if (value === 5) return "普通";
                 if (value === 1) return "低い/悪い";
                 return "";
-           }
-                    }
+             }
                 }
             }
         }
-    });
-}
+    }
+});
