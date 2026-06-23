28ac31451787b4615bf8a38c716b72cafc99811e
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-24 01:44:45 +0900

    Fix syntax errors in chart.js

--- js/chart.js
+++ js/chart.js
@@ -32,19 +32,17 @@ if (chartInner) {
 
         const d = new Date(l.date);
 
-        const year = d.getFullYear();、
+        const year = d.getFullYear();
         const month = d.getMonth() + 1;
         const day = d.getDate();
 
         const hour = String(d.getHours()).padStart(2, "0");
         const min = String(d.getMinutes()).padStart(2, "0");
 
-        if (index > 0) {
-            const prev = new Date(last10[index - 1].date);
 
-           const prev = last10[index - 1];
+        const prev = last10[index - 1];
 
-const showYear =
+        const showYear =
     !prev ||
     new Date(prev.date).getFullYear() !== year;
 
