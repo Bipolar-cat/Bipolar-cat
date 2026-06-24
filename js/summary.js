3922b09bb75c794f4e16649e1b57ed2634e2d6b9
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-24 15:33:00 +0900

    Comment out generateSummary function and logs

--- js/summary.js
+++ js/summary.js
@@ -1,8 +1,8 @@
- //*function generateSummary(logs) {
+ /*function generateSummary(logs) {
 
-    return "最近は安定しています";
-}
-const result = analyzeTrend(logs);*//
+   * return "最近は安定しています";
+*}
+*/const result = analyzeTrend(logs);*//
 
 function generateSummary() {
 
