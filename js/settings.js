a53d091ff512086d4dbffbf8081e0540232c8e21
Bipolar-cat | Innernote開発 <1102.okappi73@gmail.com>
2026-06-24 15:51:08 +0900

    Enhance settings.js with descriptive comments
    Added comments in Japanese to explain settings options.

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,12 @@
-　　/*settings.js ◎*/
+　　/*settings.js ◎
+*⚙ 設定
+*
+*記録方式
+*
+*○ 3段階（簡単）
+*○ 10段階（細かく記録）
+*
+*[保存]*/
 
     function toggleSettings() {
     const panel = document.getElementById("settings-panel");
